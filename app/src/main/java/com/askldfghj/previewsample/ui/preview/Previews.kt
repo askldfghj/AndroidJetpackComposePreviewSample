ab351d466e428f42fc4b251f33d994701b5b5cf7
askldfghj <askldfghj@naver.com>
2023-10-05 10:47:32 +0900

    create device previews

--- app/src/main/java/com/askldfghj/previewsample/ui/preview/Previews.kt
+++ app/src/main/java/com/askldfghj/previewsample/ui/preview/Previews.kt
@@ -1,8 +1,15 @@
 package com.askldfghj.previewsample.ui.preview
 
 import android.content.res.Configuration
+import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Preview(group = "Theme", name = "Light", showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_NO)
 @Preview(group = "Theme", name = "Dark", showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
 internal annotation class ThemePreviews
+
+@Preview(group = "Device", name = "Phone", showBackground = true, device = Devices.PHONE)
+@Preview(group = "Device", name = "Tablet", showBackground = true, device = Devices.TABLET)
+@Preview(group = "Device", name = "PortraitTablet", showBackground = true, widthDp = 768, heightDp = 1024)
+internal annotation class DevicePreviews
